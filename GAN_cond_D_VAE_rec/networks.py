2aec19de5ab7c47665d10b6d3aab0fe65a33340c
Dima Lipin <dimich3d@ya.ru>
2018-03-19 19:53:33 +0300

    start work on VAE-GAN

--- GAN_cond_D_VAE_rec/networks.py
+++ GAN_cond_D_VAE_rec/networks.py
@@ -1,12 +1,9 @@
 import tensorflow as tf
-
-ds = tf.contrib.distributions
-layers = tf.contrib.layers
-tfgan = tf.contrib.gan
+from tensorflow.contrib import layers
 
 
-def pix2pix_generator(inputs, weight_decay=2.5e-5, is_training=True):
-    noise, vae_generated_images = inputs
+def generator(inputs,  weight_decay=2.5e-5, is_training=True):
+    noise, _ = inputs
     with tf.contrib.framework.arg_scope(
             [layers.fully_connected, layers.conv2d_transpose],
             activation_fn=tf.nn.relu, normalizer_fn=layers.batch_norm,
@@ -24,28 +21,23 @@ def pix2pix_generator(inputs, weight_decay=2.5e-5, is_training=True):
                 net, 1, [4, 4], normalizer_fn=None, activation_fn=tf.tanh)
             return net
 
-def _discriminator_helper(img, is_conditional, one_hot_labels, weight_decay):
-  """Core MNIST discriminator.
-  This function is reused between the different GAN modes (unconditional,
-  conditional, etc).
-  Args:
-    img: Real or generated MNIST digits. Should be in the range [-1, 1].
-    is_conditional: Whether to condition on labels.
-    one_hot_labels: Labels to optionally condition the network on.
-    weight_decay: The L2 weight decay.
-  Returns:
-    Final fully connected discriminator layer. [batch_size, 1024].
-  """
-  with tf.contrib.framework.arg_scope(
-      [layers.conv2d, layers.fully_connected],
-      activation_fn=_leaky_relu, normalizer_fn=None,
-      weights_regularizer=layers.l2_regularizer(weight_decay),
-      biases_regularizer=layers.l2_regularizer(weight_decay)):
-    net = layers.conv2d(img, 64, [4, 4], stride=2)
-    net = layers.conv2d(net, 128, [4, 4], stride=2)
-    net = layers.flatten(net)
-    if is_conditional:
-      net = tfgan.features.condition_tensor_from_onehot(net, one_hot_labels)
-    net = layers.fully_connected(net, 1024, normalizer_fn=layers.layer_norm)
 
-    return net
+def discriminator(img, inputs, weight_decay=2.5e-5):
+    _, img_rec = inputs
+    with tf.contrib.framework.arg_scope(
+            [layers.conv2d, layers.fully_connected],
+            activation_fn= lambda x: tf.nn.leaky_relu(x, alpha=0.01), normalizer_fn=None,
+            weights_regularizer=layers.l2_regularizer(weight_decay),
+            biases_regularizer=layers.l2_regularizer(weight_decay)):
+        net = layers.conv2d(img, 64, [4, 4], stride=2)
+        net = layers.conv2d(net, 128, [4, 4], stride=2)
+        net = layers.flatten(net)
+        net = layers.fully_connected(net, 1024, normalizer_fn=layers.layer_norm)
+
+        add_net = layers.conv2d(img_rec, 32, [4, 4], stride=2)
+        add_net = layers.conv2d(add_net, 64, [4, 4], stride=2)
+        add_net = layers.flatten(add_net)
+        add_net = layers.fully_connected(add_net, 512, normalizer_fn=layers.layer_norm)
+
+        net = layers.linear(tf.concat([net, add_net], -1), 1)
+        return net
--- GAN_cond_D_VAE_rec/train.py
+++ GAN_cond_D_VAE_rec/train.py
@@ -1,20 +1,18 @@
-import functools
-
 import tensorflow as tf
-
-# import data_provider
 import networks
 # import util
-from gan_tf_examples.mnist import data_provider
-flags = tf.flags
-tfgan = tf.contrib.gan
+from tensorflow import flags
+import tensorflow.contrib.gan as tfgan
+from gan_tf_examples.mnist.data_provider import provide_data
+from VAE.variational_autoencoder import VAE
 
-flags.DEFINE_integer('batch_size', 32, 'The number of images in each batch.')
 
-flags.DEFINE_string('train_log_dir', '/tmp/mnist/',
+flags.DEFINE_integer('batch_size', 64, 'The number of images in each batch.')
+
+flags.DEFINE_string('train_log_dir', 'mnist_norm_images',
                     'Directory where to write event logs.')
 
-flags.DEFINE_string('dataset_dir', None, 'Location of data.')
+flags.DEFINE_string('dataset_dir', "../data/mnist", 'Location of data.')
 
 flags.DEFINE_integer('max_number_of_steps', 20000,
                      'The maximum number of gradient steps.')
@@ -32,15 +30,6 @@ flags.DEFINE_integer(
 FLAGS = flags.FLAGS
 
 
-def _learning_rate(gan_type):
-    # First is generator learning rate, second is discriminator learning rate.
-    return {
-        'unconditional': (1e-3, 1e-4),
-        'conditional': (1e-5, 1e-4),
-        'infogan': (0.001, 9e-5),
-    }[gan_type]
-
-
 def main(_):
     if not tf.gfile.Exists(FLAGS.train_log_dir):
         tf.gfile.MakeDirs(FLAGS.train_log_dir)
@@ -49,54 +38,58 @@ def main(_):
     # the forward inference and back-propagation.
     with tf.name_scope('inputs'):
         with tf.device('/cpu:0'):
-            images, one_hot_labels, _ = data_provider.provide_data(
-                'train', FLAGS.batch_size, FLAGS.dataset_dir, num_threads=4)
+            images, one_hot_labels, _ = provide_data('train', FLAGS.batch_size, FLAGS.dataset_dir, num_threads=4)
+            images = 2.0 * images - 1.0
 
+    rec = None
 
-    noise = tf.random_normal([FLAGS.batch_size, FLAGS.noise_dims])
     gan_model = tfgan.gan_model(
-        generator_fn=networks.conditional_generator,
-        discriminator_fn=networks.conditional_discriminator,
+        generator_fn=networks.generator,
+        discriminator_fn=networks.discriminator,
         real_data=images,
-        generator_inputs=(noise, one_hot_labels))
-    tfgan.eval.add_gan_model_image_summaries(gan_model, FLAGS.grid_size)
+        generator_inputs=[tf.random_normal(
+            [FLAGS.batch_size, FLAGS.noise_dims]), rec])
 
-# Get the GANLoss tuple. You can pass a custom function, use one of the
-# already-implemented losses from the losses library, or use the defaults.
-with tf.name_scope('loss'):
-    mutual_information_penalty_weight = (1.0 if FLAGS.gan_type == 'infogan'
-                                         else 0.0)
-    gan_loss = tfgan.gan_loss(
-        gan_model,
-        gradient_penalty_weight=1.0,
-        mutual_information_penalty_weight=mutual_information_penalty_weight,
-        add_summaries=True)
-    tfgan.eval.add_regularization_loss_summaries(gan_model)
+    tfgan.eval.add_gan_model_image_summaries(gan_model, FLAGS.grid_size, False)
 
-# Get the GANTrain ops using custom optimizers.
-with tf.name_scope('train'):
-    gen_lr, dis_lr = _learning_rate(FLAGS.gan_type)
-    train_ops = tfgan.gan_train_ops(
-        gan_model,
-        gan_loss,
-        generator_optimizer=tf.train.AdamOptimizer(gen_lr, 0.5),
-        discriminator_optimizer=tf.train.AdamOptimizer(dis_lr, 0.5),
-        summarize_gradients=True,
-        aggregation_method=tf.AggregationMethod.EXPERIMENTAL_ACCUMULATE_N)
+    # Get the GANLoss tuple. You can pass a custom function, use one of the
+    # already-implemented losses from the losses library, or use the defaults.
+    with tf.name_scope('loss'):
 
-# Run the alternating training loop. Skip it if no steps should be taken
-# (used for graph construction tests).
-status_message = tf.string_join(
-    ['Starting train step: ',
-     tf.as_string(tf.train.get_or_create_global_step())],
-    name='status_message')
-if FLAGS.max_number_of_steps == 0: return
-tfgan.gan_train(
-    train_ops,
-    hooks=[tf.train.StopAtStepHook(num_steps=FLAGS.max_number_of_steps),
-           tf.train.LoggingTensorHook([status_message], every_n_iter=10)],
-    logdir=FLAGS.train_log_dir,
-    get_hooks_fn=tfgan.get_joint_train_hooks())
+        gan_loss = tfgan.gan_loss(
+            gan_model,
+            gradient_penalty_weight=1.0,
+            mutual_information_penalty_weight=0.0,
+            add_summaries=True)
+        # tfgan.eval.add_regularization_loss_summaries(gan_model)
+
+    # Get the GANTrain ops using custom optimizers.
+    with tf.name_scope('train'):
+        gen_lr, dis_lr = (1e-3, 1e-4)
+        train_ops = tfgan.gan_train_ops(
+            gan_model,
+            gan_loss,
+            generator_optimizer=tf.train.AdamOptimizer(gen_lr, 0.5),
+            discriminator_optimizer=tf.train.AdamOptimizer(dis_lr, 0.5),
+            summarize_gradients=False,
+            aggregation_method=tf.AggregationMethod.EXPERIMENTAL_ACCUMULATE_N)
+
+    # Run the alternating training loop. Skip it if no steps should be taken
+    # (used for graph construction tests).
+    status_message = tf.string_join(
+        ['Starting train step: ',
+         tf.as_string(tf.train.get_or_create_global_step())],
+        name='status_message')
+    if FLAGS.max_number_of_steps == 0:
+        return
+    tfgan.gan_train(
+        train_ops,
+        hooks=[tf.train.StopAtStepHook(num_steps=FLAGS.max_number_of_steps),
+               tf.train.LoggingTensorHook([status_message], every_n_iter=10)],
+        logdir=FLAGS.train_log_dir,
+        save_summaries_steps=500,
+        get_hooks_fn=tfgan.get_joint_train_hooks())
+
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
